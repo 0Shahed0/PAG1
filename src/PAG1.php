23b180305f12da5cad6f6c0941bfe663c1bf94c2
0Shahed0 <126228343+0Shahed0@users.noreply.github.com>
2023-03-09 21:15:59 +0300

    Update PAG1.php
    shahed

--- src/PAG1.php
+++ src/PAG1.php
@@ -4,38 +4,55 @@
      age(integer), 
      and isStudent(boolean).*/
 
-$name = null;
+$name = shahed;
 
 
-$age = null;
+$age = 20;
 
 
-$isStudent = null;
+$isStudent = True;
 
 /*task2: Create an indexed array named (numbers) and an associative array named (person), 
 each with at least 5 elements.*/
 
-$numbers = null;
+$numbers = [1,2 , 3,4,5 ];
 
 
-$person = null;
+$person = ["name"=>"shahed" , "age" =>"20" , "job" =>"student" ,"student" =>"MECA" ,"number"=>"5314856035"]
 
 /* task3: Write a conditional statement using if-else to check if age declared in task 1 is odd or even, 
 look (result) variable and assign to it: 
 - "Your age is even" in case of even 
 - "Your age is odd" in case of odd.*/
-
+ 
 $result = '';
 // your code here..
 
+if ($age %2==0){
+      $result = 'Your age is even';
+     }else{
+     $result = 'Your age is ODD';}
+     
 
 /* task4: Use some kind of loop to iterate over each element in the indexed array(numbers) created in task 2 , 
 - look (oddarr) : it's new array should contains each odd element.*/
 $oddarr = [];
 // your code here..
-
+foreach($numbers as $number){
+     if ($umber %2!=0){
+          $oddarr []=$nmber;}
+}
+     
 /* task5:Write a custom function named: 
 (repeatString) that takes 2 arguments, an integer and a string, 
 and returns the string repeated the number of times specified by the integer.*/
 
 // your code here..
+function repeatString( $num , $str  ){
+     for ($i=1 , $i <=  $sum , $i++){
+          $result.=$str;
+          }
+     return $result;}
+
+    
+ 
